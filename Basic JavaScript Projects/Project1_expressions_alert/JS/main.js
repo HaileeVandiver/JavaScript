738b7aacab3eb88ee6747164b44250589e934e0c
HaileeVandiver <haileevandiver@gmail.com>
2021-06-01 14:43:12 -0700

    project1 expressions

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+var daughter= "Hailee", mother="Tami", brotherOne= "Brent", brotherTwo="Drew"; // define variables
+var daughter = daughter.fontcolor("pink");//give var duaghter the color pink
+var mother = mother.fontcolor("purple");
+var brotherOne = brotherOne.fontcolor("black");
+var brotherTw0 = brotherTwo.fontcolor("red")
+document.write(daughter + mother);// return daughter and mother 
+winodws.alert("Hailee made dis")
