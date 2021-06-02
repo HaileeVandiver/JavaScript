a0f6daa7d9b9950bdcfb81e2141869a3e992363a
HaileeVandiver <haileevandiver@gmail.com>
2021-06-02 12:57:58 -0700

    corrections

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -3,27 +3,27 @@ var subtraction = 2 - 2;  // these are the things I'm subtracting
 document.getElementById("Math").innerHTML= "2 - 2 = " + subtraction;// this is what displays plus the actual answer 
 }
 
-function multiplication() { 
-    var simple_Math = 6 * 8; 
-    document.getElementById("MathM").innerHTML = "6 x 8 = " + simple_Math;
+function multiplication() { // I'm a function that multiplies
+    var simple_Math = 6 * 8; // here is what I am going to multiply 
+    document.getElementById("MathM").innerHTML = "6 x 8 = " + simple_Math;// and here is how it will display
 }
 
-function division() {
-    var simple_Division = 48 / 6 ;
-    document.getElementById("MathD").innerHTML = "48 / 6 = "+ simple_Division;
+function division() {//im a function that divides 
+    var simple_Division = 48 / 6 ;// here is what I will divide
+    document.getElementById("MathD").innerHTML = "48 / 6 = "+ simple_Division;// and here is how it will dispaly 
 }
 
-function more_Math() {
-    var simple_Math = (1 + 2) * 10 / 2 - 5; 
+function more_Math() {// im a function that does a bunch of math 
+    var simple_Math = (1 + 2) * 10 / 2 - 5; // here is that math that i'm going to do 
     document.getElementById("allTheMath").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals" + simple_Math;
 }
 
-function modulus_Operator() { 
+function modulus_Operator() { // oh man look another math function! Wild 
     var ednaMode = 2500 % 6; 
     document.getElementById("EdnaMode").innerHTML = "When Edna Mode makes super hero capes she has " + ednaMode + " feet of a bolt of fabric left over" ;
 }
 
-function negation_Operator() {
+function negation_Operator() {// we're getting crazy up in these comments 
     var x = 10; 
     document.getElementById("negativeMath").innerHTML= "negative hehehe " + -x; 
 }
