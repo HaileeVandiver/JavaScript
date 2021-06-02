ea73c5c940fa7fe4b6fd8960fc3a8ffa3b3bb7d8
HaileeVandiver <haileevandiver@gmail.com>
2021-06-02 14:09:12 -0700

    comparison types

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,1 +1,35 @@
-document.write(typeof 5);
+document.write(typeof 5); // shows data type
+document.write("10" + 5); // combines a string and a number 
+
+
+// boolean logic 
+document.write(10>5);
+document.write( 5 > 10); 
+document.write( 5 > 2 && 10 > 4);
+document.write( 5 > 10 && 10 > 4); 
+document.write( 5 > 10 || 10 >4); 
+document.write( 5 > 10 || 10 > 20); 
+
+//equality 
+
+document.write (10 == 10);
+document.write (10 == 3); 
+
+// === equal in both value and data type
+var x = 10 ;
+var z = 10;
+var y = 9;
+document.write( x === z);
+
+var hai = "Hailee";
+var sco = "Scott";
+
+document.write(z === sco);
+document.write (z === y); 
+document.write (hai == sco);
+
+// console log 
+console.log(2 + 2);
+console.log(5 > 10);
+console.log(10 > 5);
+
