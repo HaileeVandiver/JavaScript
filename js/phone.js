71610e031bdc8ac606dc1eda0541c19fbbb80708
HaileeVandiver <haileevandiver@gmail.com>
2021-06-17 09:32:43 -0700

    final javascript projects

---
+++ js/phone.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("phoneForm").style.display = "block";
+}
+
+function closeForm(){
+    document.getElementById("phoneForm").style.display = "none";
+}
